0ee0306dc76f57b355ffe9d9bdc714fec563cd30
VladOSIO2 <vl4ds1mus@gmail.com>
2022-07-24 21:12:19 +0300

    made User entity a class, not a record

--- src/entities/User.java
+++ src/entities/User.java
@@ -2,5 +2,40 @@ package entities;
 
 import java.math.BigDecimal;
 
-public record User(int id, String firstName, String lastName, BigDecimal moneyAmount) {
+public class User {
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private BigDecimal moneyAmount;
+
+    public User(int id, String firstName, String lastName, BigDecimal moneyAmount) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    public boolean spendMoney(BigDecimal moneyToSpend) {
+        boolean hasMoneyAmount = moneyAmount.compareTo(moneyToSpend) >= 0;
+        if (hasMoneyAmount) {
+            moneyAmount = moneyAmount.subtract(moneyToSpend);
+        }
+        return hasMoneyAmount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public BigDecimal getMoneyAmount() {
+        return moneyAmount;
+    }
 }
