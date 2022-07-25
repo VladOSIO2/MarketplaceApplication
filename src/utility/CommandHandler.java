798464ba21e6fd2d37fd11fc7a2338bc0f4c9777
VladOSIO2 <vl4ds1mus@gmail.com>
2022-07-25 09:19:21 +0300

    moved NumberFormatException handling to main handle method

--- src/utility/CommandHandler.java
+++ src/utility/CommandHandler.java
@@ -29,68 +29,57 @@ public class CommandHandler {
         if (args.length < 1) {
             return "Command must not be empty!";
         }
-        return switch (args[0]) {
-            case "/help" -> HELP_MSG;
-            case "/users_list" -> DBEmulator.getUsersAsString();
-            case "/products_list" -> DBEmulator.getProductsAsString();
-            case "/purchase" -> handlePurchase(args);
-            case "/add_user" -> handleAddUser(args);
-            case "/add_product" -> handleAddProduct(args);
-            case "/delete_product" -> handleDeleteProduct(args);
-            default -> "Unknown command: " + args[0];
-        };
+        try {
+            return switch (args[0]) {
+                case "/help" -> HELP_MSG;
+                case "/users_list" -> DBEmulator.getUsersAsString();
+                case "/products_list" -> DBEmulator.getProductsAsString();
+                case "/purchase" -> handlePurchase(args);
+                case "/add_user" -> handleAddUser(args);
+                case "/add_product" -> handleAddProduct(args);
+                case "/delete_product" -> handleDeleteProduct(args);
+                default -> "Unknown command: " + args[0];
+            };
+        } catch (NumberFormatException e) {
+            return "Number parsing error: " + e.getMessage();
+        }
     }
 
     private static String handlePurchase(String[] args) {
         if (args.length < 3) {
             return NOT_ENOUGH_ARGS_MSG;
         }
-        int userID, productID;
-        try {
-            userID = Integer.parseInt(args[1]);
-            productID = Integer.parseInt(args[2]);
-        } catch (NumberFormatException e) {
-            return "Error: both user and product IDs must be positive ints!";
-        }
-        return DBEmulator.addPurchase(userID, productID);
+        return DBEmulator.addPurchase(
+                Integer.parseInt(args[1]),  //userID
+                Integer.parseInt(args[2])   //productID
+        );
     }
 
     private static String handleAddUser(String[] args) {
         if (args.length < 4) {
             return NOT_ENOUGH_ARGS_MSG;
         }
-        BigDecimal money;
-        try {
-            money = new BigDecimal(args[3]);
-        } catch (NumberFormatException e) {
-            return "Can't parse into number: " + args[3];
-        }
-        return DBEmulator.addUser(args[1], args[2], money);
+        return DBEmulator.addUser(
+                args[1],                    //first name
+                args[2],                    //last name
+                new BigDecimal(args[3])     //money
+        );
     }
 
     private static String handleAddProduct(String[] args) {
         if (args.length < 3) {
             return NOT_ENOUGH_ARGS_MSG;
         }
-        BigDecimal price;
-        try {
-            price = new BigDecimal(args[2]);
-        } catch (NumberFormatException e) {
-            return "Can't parse into number: " + args[2];
-        }
-        return DBEmulator.addProduct(args[1], price);
+        return DBEmulator.addProduct(
+                args[1],                //name
+                new BigDecimal(args[2]) //price
+        );
     }
 
     private static String handleDeleteProduct(String[] args) {
         if (args.length < 2) {
             return NOT_ENOUGH_ARGS_MSG;
         }
-        int id;
-        try {
-            id = Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-            return "Can't parse into number: " + args[1];
-        }
-        return DBEmulator.deleteProductByID(id);
+        return DBEmulator.deleteProductByID(Integer.parseInt(args[1]));
     }
 }
